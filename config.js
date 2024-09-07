4ed8164ea8b81505ddb89567b33215492a38f95d
Doc Wei <19955102698@163.com>
2024-09-07 17:35:48 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,21 +3,26 @@ var config = {
     // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
     texts: [
-        "送给我",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
-        "小可爱",  // 同上...
-        "今天",
-        "2024年9月17日",
-        "是你的生日",
-        "这是我们认识之后的",
-        "第二个生日",
-        "这一年多以来",
-        "我见过你",
-        "美丽的样子",
-        "落落大方的样子",
-        "还有....额...",
-        "奇怪的样子",
-        "不过都是让人喜欢的样子",
+        "Give me",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+        "Dear little cutie",  // 同上...
+        "Today",
+        "2024.9.17",
+        "Miss Cheng's Birthday",
+        "It's after we got to know each other",
+        "Second birthday",
+        "Over the past year or so",
+        "I have saw your",
+        "Beautiful",
+        "Dignified",
+        "And....emmm",
+        "Something strang",
+        "But they are all the looks I like",
         "YAMI~~",
+        "Happy Birthday!",
+        "As well as Mid-autumn Festival",
+        "Have you received all the gifts?",
+        "The moom is very round",
+        "Don't be lonely",
     ],
     /**
      * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
@@ -29,10 +34,10 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        "心爱的小可爱": "./imgs/xiaokeai.png",
-        //"美丽的样子":
-        //"落落大方的样子":
-        //"奇怪的样子":
+        "Dear little cutie": "./imgs/xiaokeai.png",
+        "Beautiful":"./imgs/xiaokeai.png",
+        "Dignified":"./imgs/xiaokeai.png",
+        "something strang":"./imgs/xiaokeai.png",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
